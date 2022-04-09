c6ba6f81c540df8b2e1b777097c83b0f89d868be
Kovalev Ivan <j.j.cherdak@gmail.com>
2022-04-09 19:38:10 +0300

    was changed SearchPresenter.kt

--- app/src/main/java/com/geekbrains/tests/presenter/search/SearchSchedulerProvider.kt
+++ app/src/main/java/com/geekbrains/tests/presenter/search/SearchSchedulerProvider.kt
@@ -5,7 +5,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import com.geekbrains.tests.presenter.SchedulerProvider
 
-internal class SearchSchedulerProvider {
+internal class SearchSchedulerProvider: SchedulerProvider {
     override fun ui(): Scheduler {
         return AndroidSchedulers.mainThread()
     }
--- app/src/main/java/com/geekbrains/tests/view/search/MainActivity.kt
+++ app/src/main/java/com/geekbrains/tests/view/search/MainActivity.kt
@@ -2,8 +2,6 @@ package com.geekbrains.tests.view.search
 
 import android.os.Bundle
 import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.geekbrains.tests.BuildConfig
